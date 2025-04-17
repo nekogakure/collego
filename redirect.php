3da384d67fb76f1f221fe0ca6fa1d3ef32e9ce93
ねこがくれ <osjin83@gmail.com>
2025-04-17 21:27:54 +0900

    init

---
+++ redirect.php
@@ -0,0 +1,12 @@
+<?php
+    $rand = rand(0, 1);
+    $GUI_URL = "gui.html";
+    $MS_URL = "ms.html";
+
+    if ($rand == 0) {
+        $content = file_get_contents($GUI_URL);
+        echo $content;
+    } else {
+        $content = file_get_contents($MS_URL);
+        echo $content;
+    }
